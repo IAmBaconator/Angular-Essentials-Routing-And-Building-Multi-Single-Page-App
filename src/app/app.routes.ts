ac7ef0474d96a43d1e2636884e6e41d68bbd9246
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-11-18 19:49:50 +0000

    Exporting Routing breaks the routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
+
+// Using this style appears to break the routing system by pulling it from main.ts
 import { Routes } from "@angular/router";
 
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { TaskComponent } from "./tasks/task/task.component";
 
 export const routes: Routes = [
-    {
-        path: 'tasks', // <your-domain>/tasks
-        component: TaskComponent,
-    },
-];
+            {
+                path: '', // <your-domain>/
+                component: NoTaskComponent,
+            },
+            {
+                path: 'tasks', // <your-domain>/tasks
+                component: TaskComponent,
+            },
+        ];
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { NoTaskComponent } from './app/tasks/no-task/no-task.component';
+import { TaskComponent } from './app/tasks/task/task.component';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => 
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideRouter([
+            {
+                path: '', // <your-domain>/
+                component: NoTaskComponent,
+            },
+            {
+                path: 'tasks', // <your-domain>/tasks
+                component: TaskComponent,
+            },
+        ]),
+    ], // Route = path + information associated with the path you wish Angular to load when the path is presented.
+}).catch((err) => 
     console.error(err));
