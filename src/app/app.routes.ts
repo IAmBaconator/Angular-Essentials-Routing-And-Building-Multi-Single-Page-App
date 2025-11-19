c78b821cbe53133da7d19882258acc2e241af5e2
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-11-19 20:44:34 +0000

    Introduction to Resolver to help pull jobs off the component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { Routes } from "@angular/router";
 
 import { routes as userRoutes } from "./users/user/users.routes";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
-import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
@@ -18,7 +18,10 @@ export const routes: Routes = [
                 children: userRoutes,
                 data: {
                     message: 'Hello!' // Option allowing you to setup any static value that is passed as a property to the associated component IF you have withComponentInputBinding() enabled.
-                }
+                },
+                resolve: {
+                    userName: resolveUserName // No need to add () to execute resolveUserName because Angular does this for you.
+                } // Works like the data parameter but deals with dynamic values.
             },
             {
                 path: '**',
