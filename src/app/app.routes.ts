3328c2f333f91b678f2d28bff4e481ee62fbe76b
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-11-19 23:19:00 +0000

    Adding Route Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,33 @@
 
 // Using this style appears to break the routing system by pulling it from main.ts
-import { Routes } from "@angular/router";
+import { CanMatch, CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 
 import { routes as userRoutes } from "./users/users.routes";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
+import { inject } from "@angular/core";
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+    const router = inject(Router);
+    const shouldGetAccess = Math.random();
+    if (shouldGetAccess < 0.5) {
+        return true;
+    }
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
+} // Similar formating as the ResolverFN
 
 export const routes: Routes = [
             {
                 path: '', // <your-domain>/
                 component: NoTaskComponent,
-                title: '', // To show up in browser title and searched results.
+                title: 'No task selected', // To show up in browser title and searched results.
             },
             {
                 path: 'users/:userId', // <your-domain>/users/<uid>
                 component: UserTasksComponent,
                 children: userRoutes,
+                canMatch: [dummyCanMatch], // Allows you control whether the entire route should be matched by a certain navigational route.
                 data: {
                     message: 'Hello!' // Option allowing you to setup any static value that is passed as a property to the associated component IF you have withComponentInputBinding() enabled.
                 },
